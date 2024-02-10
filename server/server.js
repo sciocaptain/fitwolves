0a08c52ac4f7c50bef37b33bac109b6c6445106d
sciocaptain <67564019+sciocaptain@users.noreply.github.com>
2024-02-09 22:14:02 -0500

    pushing things from before will be back soon

--- server/server.js
+++ server/server.js
@@ -2,37 +2,26 @@ var mongoose = require("mongoose")
 var express = require("express")
 var cors = require('cors')
 const dotenv = require('dotenv').config()
-var bcrypt = require('bcrypt')
-var ExpressSession = require("express-session")
-var SessionStore = require('connect-mongodb-session')(ExpressSession);
-var keySecret = process.argv[2];
 const connectDB = require('./config/db')
 
 connectDB()
 
 var server = express()
-var MongoDB = "mongodb+srv://ormstats1:ORMdb2024@orm.cmeeoo3.mongodb.net/?retryWrites=true&w=majority"
+var MongoDB = process.env.MONGO_URI
 
-/* THIS IS WHERE WE SHOULD START CODING */
+const ServerV = server.listen(8000, () => {
+    console.log("Server is running on port 8000")
+})
 
-
-
-
-
-/* DON'T ADD ANY CODE AFTER THIS */
-
-
-// Start the server
-const port = 8000;
-const server = app.listen(port, () => {console.log(`Server is running on https://localhost:${port}`);});
-
-// Handle server termination
-process.on('SIGINT', () => {
-    server.close(() => {
-      mongoose.disconnect().then(() => {
-        console.log('\nServer closed. Database instance disconnected');
-        process.exit(0);
-      });
+process.on("SIGINT", () => {
+    ServerV.close(() => {
+        console.log("Server closed. Database instance disconnected")
+        process.exit(0)
     });
-  });
-  
+});
+
+server.use(cors({
+    origin: true,
+    credentials: true
+}))
+server.use(express.json())
