7ca048f237bce5ef68840d4f7a32af197ab64a40
sciocaptain <67564019+sciocaptain@users.noreply.github.com>
2024-02-09 23:17:32 -0500

    changes

--- server/server.js
+++ server/server.js
@@ -3,11 +3,16 @@ var express = require("express")
 var cors = require('cors')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
+const axios = require('axios');
 
 connectDB()
 
 var server = express()
 var MongoDB = process.env.MONGO_URI
+
+/*CODE BELOW*/
+ 
+/*CODE ABOVE*/
 
 const ServerV = server.listen(8000, () => {
     console.log("Server is running on port 8000")
