c0d3721780307809c2d12a47c9b838e95d38eba4
sciocaptain <67564019+sciocaptain@users.noreply.github.com>
2024-02-10 02:22:16 -0500

    food model added

--- server/server.js
+++ server/server.js
@@ -9,9 +9,29 @@ connectDB()
 
 var server = express()
 var MongoDB = process.env.MONGO_URI
+var Food = require("./models/food.js")
 
 /*CODE BELOW*/
- 
+
+async function saveFoodItem() {
+    const foodItem = new Food({
+        name: 'Spaghetti with Meatballs',
+        date: new Date('2024-02-09'), // Assuming today's date
+        type: 'dinner',
+        carbs: 50,
+        protein: 20,
+        fat: 15,
+        calories: 500,
+        vegetarian: false
+    });
+
+    // Save the food item to the database
+    await foodItem.save();
+}
+
+// Call the asynchronous function
+saveFoodItem();
+
 /*CODE ABOVE*/
 
 const ServerV = server.listen(8000, () => {
