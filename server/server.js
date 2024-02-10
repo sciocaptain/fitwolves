1f913a63c88017ea9661e320005a8029b5eaa326
sciocaptain <67564019+sciocaptain@users.noreply.github.com>
2024-02-10 02:58:18 -0500

    chatgpt working

--- server/server.js
+++ server/server.js
@@ -4,12 +4,17 @@ var cors = require('cors')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
 const axios = require('axios');
+const OpenAI= require("openai")
 
 connectDB()
 
 var server = express()
 var MongoDB = process.env.MONGO_URI
 var Food = require("./models/food.js")
+
+const openai = new OpenAI({
+    apiKey: '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'// This is also the default, can be omitted
+});
 
 /*CODE BELOW*/
 
@@ -31,6 +36,23 @@ async function saveFoodItem() {
 
 // Call the asynchronous function
 saveFoodItem();
+
+// Constructing the prompt
+const input = 'What are the colors in the rainbows';
+
+async function chatGPTReponse(){
+    const response = await openai.chat.completions.create({
+
+        model: 'gpt-3.5-turbo',
+    
+        messages: [{"role":"user", "content":input}],
+    
+      })
+    
+      console.log(response.choices[0].message.content)
+}
+
+chatGPTReponse();
 
 /*CODE ABOVE*/
 
