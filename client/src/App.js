76771e43fd28cffef61030aa3e0dfdf6c14336e3
sciocaptain <67564019+sciocaptain@users.noreply.github.com>
2024-02-10 03:29:56 -0500

    front end boiler plate and navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './stylesheets/App.css';
+import Login from './pages/login'; // Adjusted component name and import path
+import MainPage from './pages/mainpage'; // Adjusted component name and import path
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login />
+      <MainPage />
+
+      <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/mainpage" element={<MainPage />} />
+        </Routes>
     </div>
   );
 }
---
+++ client/src/pages/mainpage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const mainpage = () => {
+    return (
+        <div>
+            <h1>Main Page!</h1>
+        </div>
+    );
+};
+
+export default mainpage;
